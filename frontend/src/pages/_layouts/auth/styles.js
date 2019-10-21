f3023de8e25ce2997d458b1e077ca4cc95fa1013
Leonardo Motta <mail@leomotta.me>
2019-10-21 13:46:45 -0300

    refactor:remove button css from layout

--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
@@ -38,22 +37,6 @@ export const Content = styled.div`
       color: #fb6f91;
       align-self: flex-start;
       margin: 0 0 10px;
-    }
-
-    button {
-      margin: 0 0 10px;
-      height: 44px;
-      background: #3b9eff;
-      font-weight: bold;
-      color: #fff;
-      border: 0;
-      border-radius: 4px;
-      font-size: 16px;
-      transition: background 0.2s;
-
-      &:hover {
-        background: ${darken(0.03, '#3b9eff')};
-      }
     }
 
     a {
